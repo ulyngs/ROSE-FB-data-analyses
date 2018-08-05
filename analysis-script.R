6e88d5bb4c0e1120ab15ea2f8984bda9f153da0c
ULRIK LYNGS <ulyngs@ULRIKs-MacBook-Pro.local>
2018-08-05 14:19:14 +0200

    add time spent per day and mean time spent per visit

--- analysis-script.R
+++ analysis-script.R
@@ -27,6 +27,7 @@ for (i in 1:(nrow(analysisData)-1)) {
 }
 
 #### STATS AND VISUALISATIONS ####
+### Number of visits
 # number of visits in total
 total_visits <- analysisData %>%
   filter(value.active == TRUE) %>%
@@ -42,10 +43,30 @@ visits_per_day %>%
     geom_point() + 
     geom_line()
 
-# duration in total
+visits_per_day %>%
+  ggplot(aes(x = day, y = num_visits)) +
+  geom_bar(stat = "identity")
 
-# duration per day
+### time spent
+# time spent in total (seconds)
+analysisData %>%
+  filter(value.active == TRUE) %>%
+  summarise(total_duration = sum(duration) %>% seconds_to_period() %>% round())
+
+# time spent per day (seconds)
+time_per_day <- analysisData %>%
+  group_by(day = floor_date(dateString, "day")) %>%
+  filter(value.active == TRUE) %>%
+  summarise(total_duration = sum(duration)) %>%
+  mutate(period = seconds_to_period(total_duration) %>% round()) %>%
+  mutate(short_date = str_c(year(day), "/", month(day), "/", day(day)))
+
+time_per_day %>%
+  ggplot(aes(x = short_date, y = total_duration)) +
+    geom_bar(stat = "identity") + 
+    scale_y_continuous(breaks=seq(0,1000,60)) +
+    coord_flip()
 
 # average duration per visit
-
+help("lubridate-package")
 
